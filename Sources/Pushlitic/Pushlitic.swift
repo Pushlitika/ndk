c316ccf7dced50b92cadacb2d3f519160fb7ea65
“Mike” <developer@adjust.com>
2023-11-22 15:38:22 +0100

    Beta release candidate

--- Sources/Pushlitic/Pushlitic.swift
+++ Sources/Pushlitic/Pushlitic.swift
@@ -8,8 +8,8 @@ import Foundation
 import Pushlitica
 
 public struct Pushlitic {
-    public private(set) var key
-    public init(key:String) {
+    public private(set) var key:String
+    public init(_ key:String) {
         self.key = key
         Pushlitica.setup(ukey: key)
     }
